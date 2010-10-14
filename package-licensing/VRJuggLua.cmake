1e2c3d6afd4b0aa5cdeacde0c01ebb5a86d124a3
Ryan Pavlik <rpavlik@iastate.edu>
2010-10-14 10:19:31 -0500

    add licensing system

---
+++ package-licensing/VRJuggLua.cmake
@@ -0,0 +1,13 @@
+# Original Author:
+# 2009-2010 Ryan Pavlik <rpavlik@iastate.edu> <abiryan@ryand.net>
+# http://academic.cleardefinition.com
+# Iowa State University HCI Graduate Program/VRAC
+
+add_proprietary_license("VR JuggLua" "Based in part on VR JuggLua, Copyright 2010 Iowa State University. This version of VR JuggLua is unreleased research software - NOT FOR REDISTRIBUTION.")
+add_redistribution_warning("This version of VR JuggLua is unreleased research software - NOT FOR REDISTRIBUTION.")
+include("package-licensing/VRJuggler")
+include("package-licensing/OpenSceneGraph")
+include("package-licensing/Lua")
+include("package-licensing/LuaBind")
+include("package-licensing/osgLua")
+include("package-licensing/Boost")
---
+++ package-licensing/VirtuoseAPI.cmake
@@ -0,0 +1,7 @@
+# Original Author:
+# 2009-2010 Ryan Pavlik <rpavlik@iastate.edu> <abiryan@ryand.net>
+# http://academic.cleardefinition.com
+# Iowa State University HCI Graduate Program/VRAC
+
+add_proprietary_license(VirtuoseAPI "Based in part on the Haption VirtuoseAPI." "<http://www.haption.com/site/eng/html/logiciel.php?item=1>")
+add_redistribution_warning("Redistribution of the VirtuoseAPI may be restricted.")
