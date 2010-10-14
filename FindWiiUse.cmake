afeb2161211f694aef24d22b305ec0b221cf931d
Ryan Pavlik <rpavlik@iastate.edu>
2010-10-14 10:45:01 -0500

    Put BSL 1.0 license notice on all cmake files

--- FindWiiUse.cmake
+++ FindWiiUse.cmake
@@ -18,6 +18,11 @@
 # 2009-2010 Ryan Pavlik <rpavlik@iastate.edu> <abiryan@ryand.net>
 # http://academic.cleardefinition.com
 # Iowa State University HCI Graduate Program/VRAC
+#
+#          Copyright Iowa State University 2009-2010
+# Distributed under the Boost Software License, Version 1.0.
+#    (See accompanying file LICENSE_1_0.txt or copy at
+#          http://www.boost.org/LICENSE_1_0.txt)
 
 set(WIIUSE_ROOT_DIR
 	"${WIIUSE_ROOT_DIR}"
