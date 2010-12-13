2052efb5c017ed8eb51305b1604465928e5bdf41
Ryan Pavlik <rpavlik@iastate.edu>
2010-12-13 12:28:28 -0600

    Generate text too, and rename

--- update-help.sh
+++ update-help.sh
@@ -2,5 +2,6 @@
 
 src="$(dirname $(readlink -f $0))"
 : ${CMAKE:=$(which cmake)}
-$CMAKE "-DCMAKE_MODULE_PATH=$src" --help-custom-modules "$src/help.html"
+$CMAKE "-DCMAKE_MODULE_PATH=$src" --help-custom-modules "$src/module-help.html"
+$CMAKE "-DCMAKE_MODULE_PATH=$src" --help-custom-modules "$src/module-help.txt"
 
