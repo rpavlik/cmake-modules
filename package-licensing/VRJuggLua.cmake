f9a5b86501546350013d48ed5ef3cb8fd729455d
Ryan Pavlik <rpavlik@iastate.edu>
2012-03-20 11:59:22 -0500

    VR JuggLua is no longer unreleased research software - ditch the scary warning.

--- package-licensing/VRJuggLua.cmake
+++ package-licensing/VRJuggLua.cmake
@@ -1,16 +1,16 @@
 # Original Author:
-# 2009-2010 Ryan Pavlik <rpavlik@iastate.edu> <abiryan@ryand.net>
+# 2009-2012 Ryan Pavlik <rpavlik@iastate.edu> <abiryan@ryand.net>
 # http://academic.cleardefinition.com
 # Iowa State University HCI Graduate Program/VRAC
 #
-# Copyright Iowa State University 2009-2010.
+# Copyright Iowa State University 2009-2012.
 # Distributed under the Boost Software License, Version 1.0.
 # (See accompanying file ../LICENSE_1_0.txt or copy at
 # http://www.boost.org/LICENSE_1_0.txt)
 
-add_proprietary_license("VR JuggLua"
-	"Based in part on VR JuggLua, Copyright 2010 Iowa State University. This version of VR JuggLua is unreleased research software - NOT FOR REDISTRIBUTION.")
-add_redistribution_warning("This version of VR JuggLua is unreleased research software - NOT FOR REDISTRIBUTION.")
+add_permissive_license("VR JuggLua"
+	"Based in part on VR JuggLua, Copyright 2010-2012 Iowa State University. VR JuggLua is distributed under the Boost Software License, Version 1.0. See copy at http://www.boost.org/LICENSE_1_0.txt"
+	"<https://github.com/vance-group/vr-jugglua>")
 include("package-licensing/VRJuggler")
 include("package-licensing/OpenSceneGraph")
 include("package-licensing/Lua")
