503a0bbee985561bb923e2d148cbdf6a5c2f1141
Pascal Bach <pascal.bach@siemens.com>
2015-11-17 15:49:03 +0100

    Add git_semver macro
    This macro uses git_describe and parses the output accroding to semver.org
    It sets the variables CMake normally expects from a version

--- GetGitRevisionDescription.cmake
+++ GetGitRevisionDescription.cmake
@@ -18,12 +18,27 @@
 # and adjusting the output so that it tests false if there was no exact
 # matching tag.
 #
+#  git_semver(<var_prefix> [<additional arguments to git describe> ...])
+#
+# Uses git describe but tries to parse the result according to the semver standard
+# It expects tags to have the form v1.2.3 or 2.3.4. It will set the following variables:
+#
+# <var_prefix> containing the version as returned by git describe
+# <var_prefix>_MAJOR the major version
+# <var_prefix>_MINOR the minor version
+# <var_prefix>_PATCH the patch version
+#
+# <var_prefix>_SHORT the version in the form MAJOR.MINOR.PATCH
+#
 # Requires CMake 2.6 or newer (uses the 'function' command)
 #
 # Original Author:
 # 2009-2010 Ryan Pavlik <rpavlik@iastate.edu> <abiryan@ryand.net>
 # http://academic.cleardefinition.com
 # Iowa State University HCI Graduate Program/VRAC
+#
+# Contributors:
+# Pascal Bach <pascal.bach@siemens.com>
 #
 # Copyright Iowa State University 2009-2010.
 # Distributed under the Boost Software License, Version 1.0.
@@ -111,3 +126,11 @@ function(git_get_exact_tag _var)
 	git_describe(out --exact-match ${ARGN})
 	set(${_var} "${out}" PARENT_SCOPE)
 endfunction()
+
+macro(git_semver var_prefix)
+	git_describe("${var_prefix}" "${ARGN}")
+	string(REGEX REPLACE "^v?([0-9]+)\\..*" "\\1" ${var_prefix}_MAJOR "${${var_prefix}}")
+	string(REGEX REPLACE "^v?[0-9]+\\.([0-9]+).*" "\\1" ${var_prefix}_MINOR "${${var_prefix}}")
+	string(REGEX REPLACE "^v?[0-9]+\\.[0-9]+\\.([0-9]+).*" "\\1" ${var_prefix}_PATCH "${${var_prefix}}")
+	set(${var_prefix}_SHORT "${${var_prefix}_MAJOR}.${${var_prefix}_MINOR}.${${var_prefix}_PATCH}")
+endmacro()
