2c5bec74c1d9180e7d259431aa1fc926506583a3
Ryan Pavlik <ryan@sensics.com>
2016-04-25 14:42:11 -0500

    Update WinHID drastically to use FindWindowsSDK, so it can easily build against much newer SDKs/DDKs/WDKs.
    Also builds against MinGW-w64 as found in MSYS2 now, too.

--- FindWinHID.cmake
+++ FindWinHID.cmake
@@ -31,28 +31,40 @@ if(NOT WIN32)
 		WIN32)
 	return()
 endif()
+
 if(MSVC)
 	if( (NOT WINHID_ROOT_DIR) AND (NOT ENV{DDKROOT} STREQUAL "") )
 		set(WINHID_ROOT_DIR "$ENV{DDKROOT}")
 	endif()
 endif()
+
 set(WINHID_ROOT_DIR
 	"${WINHID_ROOT_DIR}"
 	CACHE
 	PATH
 	"Directory to search")
 
-if(CMAKE_SIZEOF_VOID_P MATCHES "8")
-	set(_arch amd64) # what the architecture used to be called
-	set(_arch8 x64) # what the WDK for Win8+ calls this architecture
-else()
-	set(_arch i386) # what the architecture used to be called
-	set(_arch8 x86) # what the WDK for Win8+ calls this architecture
-endif()
-
 set(_deps_check)
 set(_need_crt_dir)
 if(MSVC)
+
+	find_package(WindowsSDK)
+	set(WINSDK_LIBDIRS)
+	if(WINDOWSSDK_FOUND)
+		get_windowssdk_library_dirs_multiple(WINSDK_LIBDIRS ${WINDOWSSDK_PREFERRED_FIRST_DIRS})
+		foreach(WINSDKDIR ${WINDOWSSDK_DIRS})
+			get_windowssdk_library_dirs(${WINSDKDIR} WINSDK_CURRENT_LIBDIRS)
+			list(APPEND WINSDK_LIBDIRS ${WINSDK_CURRENT_LIBDIRS})
+		endforeach()
+	endif()
+
+	if(CMAKE_SIZEOF_VOID_P MATCHES "8")
+		set(_arch amd64) # what the architecture used to be called
+		set(_arch8 x64) # what the WDK for Win8+ calls this architecture
+	else()
+		set(_arch i386) # what the architecture used to be called
+		set(_arch8 x86) # what the WDK for Win8+ calls this architecture
+	endif()
 	include(PrefixListGlob)
 	include(CleanDirectoryList)
 	prefix_list_glob(_prefixed
@@ -65,8 +77,9 @@ if(MSVC)
 		NAMES
 		hid
 		libhid
-		HINTS
+		PATHS
 		"${WINHID_ROOT_DIR}"
+		${WINSDK_LIBDIRS}
 		${_prefixed}
 		PATH_SUFFIXES
 		"lib/w2k/${_arch}" # Win2k min requirement
@@ -81,34 +94,65 @@ if(MSVC)
 		)
 	# Might want to look close to the library first for the includes.
 	if(WINHID_LIBRARY)
+		get_filename_component(WINHID_LIBRARY_DIR "${WINHID_LIBRARY}" PATH)
+		if(WINDOWSSDK_FOUND)
+			get_windowssdk_from_component("${WINHID_LIBRARY}" _USED_WINSDK)
+			set(WINHID_LIBRARY_FROM_WINDOWSSDK ON)
+			get_windowssdk_include_dirs(${_USED_WINSDK} WINHID_INCLUDE_HINTS)
+		endif()
+	endif()
+
+	find_library(WINHID_SETUPAPI_LIBRARY
+		NAMES
+		setupapi
+		HINTS
+		"${WINHID_LIBRARY_DIR}"
+		PATHS
+		"${WINHID_ROOT_DIR}"
+		${WINSDK_LIBDIRS}
+		${_prefixed}
+		PATH_SUFFIXES
+		"lib/w2k/${_arch}" # Win2k min requirement
+		"lib/wxp/${_arch}" # WinXP min requirement
+		"lib/wnet/${_arch}" # Win Server 2003 min requirement
+		"lib/wlh/${_arch}" # Win Vista ("Long Horn") min requirement
+		"lib/wlh/um/${_arch8}" # Win Vista ("Long Horn") min requirement
+		"lib/win7/${_arch}" # Win 7 min requirement
+		"lib/win7/um/${_arch8}" # Win 7 min requirement
+		"lib/win8/${_arch}" # Win 8 min requirement
+		"lib/win8/um/${_arch8}" # Win 8 min requirement
+		)
+
+	if(WINHID_LIBRARY AND NOT WINHID_LIBRARY_FROM_WINDOWSSDK)
+		set(_basedir "${WINHID_LIBRARY_DIR}")
 		set(_prevdir)
-		get_filename_component(_basedir "${WINHID_LIBRARY}" PATH)
 		while(NOT IS_DIRECTORY "${_basedir}/lib" AND NOT (_basedir STREQUAL _prevdir))
 			set(_prevdir "${_basedir}")
 			get_filename_component(_basedir "${_basedir}/.." ABSOLUTE)
 		endwhile()
-	endif()
 
-	if(WINHID_LIBRARY AND EXISTS "${_basedir}/inc")
-		find_path(WINHID_CRT_INCLUDE_DIR # otherwise you get weird compile errors
-			NAMES
-			stdio.h
-			HINTS
-			"${_basedir}"
-			PATHS
-			"${WINHID_ROOT_DIR}"
-			PATH_SUFFIXES
-			inc/crt
-			NO_DEFAULT_PATH)
-		list(APPEND _deps_check WINHID_CRT_INCLUDE_DIR)
-		set(_need_crt_dir ON)
-	endif()
+		set(WINHID_INCLUDE_HINTS "${_basedir}")
 
+		if(EXISTS "${_basedir}/inc")
+			find_path(WINHID_CRT_INCLUDE_DIR # otherwise you get weird compile errors
+				NAMES
+				stdio.h
+				HINTS
+				"${_basedir}"
+				PATHS
+				"${WINHID_ROOT_DIR}"
+				PATH_SUFFIXES
+				inc/crt
+				NO_DEFAULT_PATH)
+			list(APPEND _deps_check WINHID_CRT_INCLUDE_DIR)
+			set(_need_crt_dir ON)
+		endif()
+	endif()
 	find_path(WINHID_INCLUDE_DIR
 		NAMES
 		hidsdi.h
 		HINTS
-		"${_basedir}"
+		${WINHID_INCLUDE_HINTS}
 		PATHS
 		"${WINHID_ROOT_DIR}"
 		PATH_SUFFIXES
@@ -119,20 +163,58 @@ if(MSVC)
 		inc/wnet
 		include/shared)
 else()
-	find_library(WINHID_LIBRARY
-		NAMES
-		libhid
-		HINTS
-		"${WINHID_ROOT_DIR}"
-		/mingw
-		PATH_SUFFIXES
-		lib
-		lib/w32api)
+	# This is the non-MSVC path.
+	if(MINGW)
+		include(MinGWSearchPathExtras)
+
+		find_library(WINHID_LIBRARY
+			NAMES
+			libhid
+			HINTS
+			"${WINHID_ROOT_DIR}"
+			${MINGWSEARCH_LIBRARY_DIRS}
+			/mingw
+			PATH_SUFFIXES
+			lib
+			lib/w32api)
+		find_library(WINHID_SETUPAPI_LIBRARY
+			NAMES
+			libsetupapi
+			HINTS
+			"${WINHID_ROOT_DIR}"
+			${MINGWSEARCH_LIBRARY_DIRS}
+			/mingw
+			PATH_SUFFIXES
+			lib
+			lib/w32api)
+	else()
+		find_library(WINHID_LIBRARY
+			NAMES
+			hid
+			libhid
+			HINTS
+			"${WINHID_ROOT_DIR}"
+			/mingw
+			PATH_SUFFIXES
+			lib
+			lib/w32api)
+		find_library(WINHID_SETUPAPI_LIBRARY
+			NAMES
+			setupapi
+			libsetupapi
+			HINTS
+			"${WINHID_ROOT_DIR}"
+			/mingw
+			PATH_SUFFIXES
+			lib
+			lib/w32api)
+	endif()
 	find_path(WINHID_INCLUDE_DIR
 		NAMES
 		hidsdi.h
 		PATHS
 		"${WINHID_ROOT_DIR}"
+		${MINGWSEARCH_INCLUDE_DIRS}
 		/mingw
 		PATH_SUFFIXES
 		include/w32api/ddk
@@ -144,34 +226,49 @@ include(FindPackageHandleStandardArgs)
 find_package_handle_standard_args(WinHID
 	DEFAULT_MSG
 	WINHID_LIBRARY
+	WINHID_SETUPAPI_LIBRARY
 	WINHID_INCLUDE_DIR
 	${_deps_check})
 
 if(WINHID_FOUND)
 	if(MSVC)
-		set(_winreq "Unknown")
-		if(WINHID_LIBRARY MATCHES "lib/w2k")
+		set(_winreq)
+		if(WINHID_LIBRARY MATCHES "[Ll]ib/w2k")
 			set(_winreq "Windows 2000")
-		elseif(WINHID_LIBRARY MATCHES "lib/wxp")
+		elseif(WINHID_LIBRARY MATCHES "[Ll]ib/wxp")
 			set(_winreq "Windows XP")
-		elseif(WINHID_LIBRARY MATCHES "lib/wnet")
+		elseif(WINHID_LIBRARY MATCHES "[Ll]ib/wnet")
 			set(_winreq "Windows Server 2003")
-		elseif(WINHID_LIBRARY MATCHES "lib/wlh")
+		elseif(WINHID_LIBRARY MATCHES "[Ll]ib/wlh")
 			set(_winreq "Windows Vista")
-		elseif(WINHID_LIBRARY MATCHES "lib/win7")
+		elseif(WINHID_LIBRARY MATCHES "[Ll]ib/win7")
 			set(_winreq "Windows 7")
-		elseif(WINHID_LIBRARY MATCHES "lib/win8")
-			set(_winreq "Windows 8 (Possibly?)")
+		elseif(WINHID_LIBRARY MATCHES "[Ll]ib/win8")
+			set(_winreq "Windows 8")
+			set(_winreq_uncertain ON)
+		elseif(WINHID_LIBRARY MATCHES "[Ll]ib/winv6.3")
+			set(_winreq "Windows 8.1")
+			set(_winreq_uncertain ON)
+		elseif(WINHID_LIBRARY MATCHES "[Ll]ib/10.0")
+			set(_winreq "Windows 10")
+			set(_winreq_uncertain ON)
 		endif()
 		if(NOT "${WINHID_MIN_WINDOWS_VER}" STREQUAL "${_winreq}")
 			if(NOT WinHID_FIND_QUIETLY)
-				message(STATUS
-					"Linking against WINHID_LIBRARY will enforce this minimum version: ${_winreq}")
+				if(NOT _winreq)
+					message("Couldn't determine if the WINHID_LIBRARY would result in a minimum version compatibility requirement.")
+				elseif(_winreq_uncertain)
+					message(STATUS
+					"Found WINHID_LIBRARY in the Windows SDK for ${_winreq} , which may or may not affect minimum compatible Windows version.")
+				else()
+					message(STATUS
+						"Linking against WINHID_LIBRARY will enforce this minimum version: ${_winreq}")
+				endif()
 			endif()
 			set(WINHID_MIN_WINDOWS_VER "${_winreq}" CACHE INTERNAL "" FORCE)
 		endif()
 	endif()
-	set(WINHID_LIBRARIES "${WINHID_LIBRARY}")
+	set(WINHID_LIBRARIES "${WINHID_LIBRARY}" "${WINHID_SETUPAPI_LIBRARY}")
 	if(_need_crt_dir)
 		set(WINHID_INCLUDE_DIRS
 			"${WINHID_CRT_INCLUDE_DIR}"
