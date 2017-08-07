ad436ee7be33ca291a59866a173b777b0e2d81ce
Ryan Pavlik <ryan@sensics.com>
2017-08-07 12:47:18 -0500

    Add creator's update windows SDK to search directories.

--- FindWindowsSDK.cmake
+++ FindWindowsSDK.cmake
@@ -73,6 +73,7 @@ macro(_winsdk_announce)
 endmacro()
 
 set(_winsdk_win10vers
+	10.0.15063.0 # Redstone 2 aka Win10 1703 "Creators' Update"
 	10.0.14393.0 # Redstone aka Win10 1607 "Anniversary Update"
 	10.0.10586.0 # TH2 aka Win10 1511
 	10.0.10240.0 # Win10 RTM
