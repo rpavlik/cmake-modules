1e1cd70abbaf47af9423507d8994b61ddfd12fd5
Matthew Russell <matthew.g.russell@gmail.com>
2018-10-06 14:58:50 -0400

    Added windows version 17134
    https://blogs.msdn.microsoft.com/chuckw/2018/05/02/windows-10-april-2018-update-sdk/

--- FindWindowsSDK.cmake
+++ FindWindowsSDK.cmake
@@ -73,6 +73,7 @@ macro(_winsdk_announce)
 endmacro()
 
 set(_winsdk_win10vers
+	10.0.17134.0 # Redstone 4 aka Win10 1803 "April 1018 Update" for VS2017
 	10.0.17133.0 # Redstone 4 aka Win10 1803 "April 1018 Update"
 	10.0.16299.0 # Redstone 3 aka Win10 1709 "Fall Creators Update"
 	10.0.15063.0 # Redstone 2 aka Win10 1703 "Creators Update"
