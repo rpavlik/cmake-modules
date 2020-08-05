43a50aab1f2d453900972ef67bf03b25f6acb800
Ryan Pavlik <ryan.pavlik@collabora.com>
2020-08-05 14:52:16 -0500

    Don't let get_git_head_revision go above the CMAKE_SOURCE_DIR, for safety.
    Think this should fix #58

--- GetGitRevisionDescription.cmake
+++ GetGitRevisionDescription.cmake
@@ -78,8 +78,16 @@ endfunction()
 
 function(get_git_head_revision _refspecvar _hashvar)
 	find_closest_git_dir("${CMAKE_CURRENT_SOURCE_DIR}" GIT_DIR)
+
+	if(NOT "${GIT_DIR}" STREQUAL "")
+		file(RELATIVE_PATH _relative_to_source_dir "${CMAKE_SOURCE_DIR}" "${GIT_DIR}")
+		if("${_relative_to_source_dir}" MATCHES "[.][.]")
+			# We've gone above the CMake root dir.
+			set(GIT_DIR "")
+		endif()
+	endif()
 	if("${GIT_DIR}" STREQUAL "")
-	   set(${_refspecvar} "GITDIR-NOTFOUND" PARENT_SCOPE)
+		set(${_refspecvar} "GITDIR-NOTFOUND" PARENT_SCOPE)
 		set(${_hashvar} "GITDIR-NOTFOUND" PARENT_SCOPE)
 		return()
 	endif()
