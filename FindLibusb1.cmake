1ff28323197d4249101c066b8bc8f2a1f329389a
Ryan Pavlik <ryan.pavlik@collabora.com>
2020-08-05 14:01:13 -0500

    Some SPDX tags

--- FindLibusb1.cmake
+++ FindLibusb1.cmake
@@ -19,6 +19,7 @@
 # Iowa State University HCI Graduate Program/VRAC
 #
 # Copyright Iowa State University 2009-2010.
+# SPDX-License-Identifier: BSL-1.0
 # Distributed under the Boost Software License, Version 1.0.
 # (See accompanying file LICENSE_1_0.txt or copy at
 # http://www.boost.org/LICENSE_1_0.txt)
